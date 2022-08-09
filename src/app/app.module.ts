f02cef70ebc25423bff0c3f1a99ff6886d4cf2d3
Roger <rogelio.g.sandoval@gmail.com>
2022-08-09 15:57:54 -0500

    add angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { RouterModule, Routes } from '@angular/router'
 import { AppearDirective } from './appear.directive';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { Home } from './components/home/home.component';
 import { Projects } from './components/projects/projects.component';
 import { Contact } from './components/contact/contact.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+
+import { AngularFireModule} from '@angular/fire/compat'
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -28,7 +35,12 @@ const routes: Routes = [
     RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
     FormsModule,
     ReactiveFormsModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase()),
+    AngularFireModule.initializeApp(environment.firebase, 'portfolio'),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'rogelio-portfolio',
+    appId: '1:460206867721:web:5b71bb685cf06a5a433fd8',
+    databaseURL: 'https://rogelio-portfolio-default-rtdb.firebaseio.com',
+    storageBucket: 'rogelio-portfolio.appspot.com',
+    apiKey: 'AIzaSyAdv5ksXKi2SLqsmyOHFSAl5aA2ZScwJAA',
+    authDomain: 'rogelio-portfolio.firebaseapp.com',
+    messagingSenderId: '460206867721',
+  },
   production: false
 };
 
