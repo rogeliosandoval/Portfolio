21800c60fb0f2e0080e6f02e24626b90d64226ad
Roger <rogelio.g.sandoval@gmail.com>
2022-08-08 21:04:33 -0500

    update the DOM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,22 +9,70 @@ export class AppComponent {
   homeActive = true;
   projectsActive = false;
   contactActive = false;
+  hiddenNav = true;
+  mobileShow = false;
+  navToggle = false;
+  navMorph = false;
+  boolean = false;
 
-  home() {
-    this.homeActive = true;
-    this.projectsActive = false;
-    this.contactActive = false;
+  @HostListener('window:scroll', ['$event']) onViewPortScroll() {
+
+    //show mobile navbar
+    if (window.pageYOffset > 200) {
+      this.mobileShow = true;
+    } else {
+      this.mobileShow = false;
+    }
+
+    //show desktop navbar background
+    if (window.pageYOffset > 20) {
+      this.navMorph = true;
+    } else {
+      this.navMorph = false;
+    }
+
+    //about me active
+    if (window.pageYOffset < 650) {
+      this.homeActive = true;
+      this.projectsActive = false;
+      this.contactActive = false;
+    }
+
+    //projects active
+    if (window.pageYOffset > 650 && window.pageYOffset < 2850) {
+      this.projectsActive = true;
+      this.homeActive = false;
+      this.contactActive = false;
+    }
+
+    //contact active
+    if (window.pageYOffset > 2850) {
+      this.contactActive = true;
+      this.homeActive = false;
+      this.projectsActive = false;
+    }
+
   }
 
-  projects() {
-    this.projectsActive = true;
-    this.homeActive = false;
-    this.contactActive = false;
+  scroll(el: HTMLElement) {
+    this.boolean = true;
+    setTimeout(() => {
+      el.scrollIntoView({ behavior: 'smooth' });
+    }, 50)
   }
 
-  contact() {
-    this.contactActive = true;
-    this.homeActive = false;
-    this.projectsActive = false;
+  scrollRef(el: ElementRef) {
+    el.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  scrollToTheTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  toggle() {
+    this.navToggle = !this.navToggle;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router'
+import { AppearDirective } from './appear.directive';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { Home } from './components/home/home.component';
 import { Projects } from './components/projects/projects.component';
+import { Contact } from './components/contact/contact.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: Home },
-  { path: 'projects', component: Projects }
+  { path: 'home', component: Home }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     Home,
-    Projects
+    Projects,
+    AppearDirective,
+    Contact
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
