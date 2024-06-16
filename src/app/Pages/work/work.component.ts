3eea38f0d32e32eab98b2c8300e674934580cd7c
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-06-16 17:55:59 -0500

    update work page

--- src/app/Pages/work/work.component.ts
+++ src/app/Pages/work/work.component.ts
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
 })
 
 export class Work {
-
+  public projects = [
+    { 
+      title: 'Rise Up Acai Bowls', 
+      description: 'An acai cafe located in San Antonio, Texas.',
+      picURL: 'rise-up.png',
+      logoURL: 'rise-up.png',
+      demoURL: 'https://riseupsatx.com/',
+      routeURL: '/projects/riseup',
+      invert: false
+    },
+    {
+      title: 'The START Center',
+      description: 'A cancer care facility with one of the most active clinical trial programs.',
+      picURL: 'start-center.png',
+      logoURL: 'start-center.png',
+      demoURL: 'https://www.thestartcenter.com/',
+      routeURL: '/projects/startcenter',
+      invert: true
+    },
+    {
+      title: 'Your Time Home Inspections',
+      description: 'A home inspection company located in San Antonio, Texas.',
+      picURL: 'ythi.png',
+      logoURL: 'ythi.png',
+      demoURL: 'https://yourtimehomeinspections.com/',
+      routeURL: '/projects/ythi',
+      invert: false
+    }
+  ]
 }
