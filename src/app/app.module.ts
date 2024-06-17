58198f280d5a858461d7b5fc0814811754a0e08f
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-06-16 21:49:29 -0500

    update home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 //Angular
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
 
 //Pages
 import { AppComponent } from './app.component'
 import { Home } from './Pages/home/home.component'
 import { Projects } from './Pages/projects/projects.component'
+import { ProjectDetails } from './Pages/project-details/project-details.component'
 import { Work } from './Pages/work/work.component'
 import { Contact } from './Pages/contact/contact.component';
 
@@ -15,10 +18,16 @@ import { Contact } from './Pages/contact/contact.component';
 import { MatDividerModule } from '@angular/material/divider'
 import { MatTooltipModule } from '@angular/material/tooltip'
 import { MatButtonModule } from '@angular/material/button'
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
 
 const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'projects', component: Projects },
+  { path: 'projects/:id', component: ProjectDetails},
   { path: 'work', component: Work },
   { path: 'contact', component: Contact},
   { path: '', redirectTo: '/home', pathMatch: 'full'}
@@ -29,15 +38,24 @@ const routes: Routes = [
     AppComponent,
     Home,
     Projects,
+    ProjectDetails,
     Work,
     Contact
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', preloadingStrategy: PreloadAllModules }),
     MatDividerModule,
     MatTooltipModule,
-    MatButtonModule
+    MatButtonModule,
+    MatExpansionModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [
     provideAnimationsAsync()
